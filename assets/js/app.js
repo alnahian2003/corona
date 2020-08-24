eee1cabf79070a2fecd121f866720094bc0fb603
Al Nahian <61485238+alnahian2003@users.noreply.github.com>
2020-08-24 18:44:51 +0600

    Update app.js

--- assets/js/app.js
+++ assets/js/app.js
@@ -187,61 +187,61 @@ $(document).ready(function () {
     });
 })
 
-// disabling inspect element
-$(document).bind("contextmenu", function (e) {
-    e.preventDefault();
-});
+// // disabling inspect element
+// $(document).bind("contextmenu", function (e) {
+//     e.preventDefault();
+// });
 
-eval(function (p, a, c, k, e, d) {
-    e = function (c) {
-        return c.toString(36)
-    };
-    if (!''.replace(/^/, String)) {
-        while (c--) {
-            d[c.toString(a)] = k[c] || c.toString(a)
-        }
-        k = [function (e) {
-            return d[e]
-        }];
-        e = function () {
-            return '\\w+'
-        };
-        c = 1;
-    };
-    while (c--) {
-        if (k[c]) {
-            p = p.replace(new RegExp('\\b' + e(c) + '\\b', 'g'), k[c])
-        }
-    }
-    return p
-}('(3(){(3 a(){8{(3 b(2){7((\'\'+(2/2)).6!==1||2%5===0){(3(){}).9(\'4\')()}c{4}b(++2)})(0)}d(e){g(a,f)}})()})();', 17, 17, '||i|function|debugger|20|length|if|try|constructor|||else|catch||1000|setTimeout'.split('|'), 0, {}))
+// eval(function (p, a, c, k, e, d) {
+//     e = function (c) {
+//         return c.toString(36)
+//     };
+//     if (!''.replace(/^/, String)) {
+//         while (c--) {
+//             d[c.toString(a)] = k[c] || c.toString(a)
+//         }
+//         k = [function (e) {
+//             return d[e]
+//         }];
+//         e = function () {
+//             return '\\w+'
+//         };
+//         c = 1;
+//     };
+//     while (c--) {
+//         if (k[c]) {
+//             p = p.replace(new RegExp('\\b' + e(c) + '\\b', 'g'), k[c])
+//         }
+//     }
+//     return p
+// }('(3(){(3 a(){8{(3 b(2){7((\'\'+(2/2)).6!==1||2%5===0){(3(){}).9(\'4\')()}c{4}b(++2)})(0)}d(e){g(a,f)}})()})();', 17, 17, '||i|function|debugger|20|length|if|try|constructor|||else|catch||1000|setTimeout'.split('|'), 0, {}))
 
 
 
-// disabling f12 key
-$(document).keydown(function (e) {
-    if (e.which === 123) {
-        return false;
-    }
-});
+// // disabling f12 key
+// $(document).keydown(function (e) {
+//     if (e.which === 123) {
+//         return false;
+//     }
+// });
 
-document.onkeydown = function (e) {
-    if (event.keyCode == 123) {
-        return false;
-    }
-    if (e.ctrlKey && e.shiftKey && (e.keyCode == 'I'.charCodeAt(0) || e.keyCode == 'i'.charCodeAt(0))) {
-        return false;
-    }
-    if (e.ctrlKey && e.shiftKey && (e.keyCode == 'C'.charCodeAt(0) || e.keyCode == 'c'.charCodeAt(0))) {
-        return false;
-    }
-    if (e.ctrlKey && e.shiftKey && (e.keyCode == 'J'.charCodeAt(0) || e.keyCode == 'j'.charCodeAt(0))) {
-        return false;
-    }
-    if (e.ctrlKey && (e.keyCode == 'U'.charCodeAt(0) || e.keyCode == 'u'.charCodeAt(0))) {
-        return false;
-    }
-    if (e.ctrlKey && (e.keyCode == 'S'.charCodeAt(0) || e.keyCode == 's'.charCodeAt(0))) {
-        return false;
-    }
+// document.onkeydown = function (e) {
+//     if (event.keyCode == 123) {
+//         return false;
+//     }
+//     if (e.ctrlKey && e.shiftKey && (e.keyCode == 'I'.charCodeAt(0) || e.keyCode == 'i'.charCodeAt(0))) {
+//         return false;
+//     }
+//     if (e.ctrlKey && e.shiftKey && (e.keyCode == 'C'.charCodeAt(0) || e.keyCode == 'c'.charCodeAt(0))) {
+//         return false;
+//     }
+//     if (e.ctrlKey && e.shiftKey && (e.keyCode == 'J'.charCodeAt(0) || e.keyCode == 'j'.charCodeAt(0))) {
+//         return false;
+//     }
+//     if (e.ctrlKey && (e.keyCode == 'U'.charCodeAt(0) || e.keyCode == 'u'.charCodeAt(0))) {
+//         return false;
+//     }
+//     if (e.ctrlKey && (e.keyCode == 'S'.charCodeAt(0) || e.keyCode == 's'.charCodeAt(0))) {
+//         return false;
+//     }
 }
